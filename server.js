d26f6c0fba3a2c4850832b7f921ff63c01adf50e
oke3016@naver.com <oke3016@naver.com>
2024-06-05 14:38:31 +0900

    최종

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 import express, { json, urlencoded } from "express";
 import cors from "cors";
 import router from "./payments.router.js";
-//import gpio from "@iiot2k/gpio";
+import gpio from "@iiot2k/gpio";
 import bodyParser from "body-parser";
 import sqlite3 from "sqlite3";
 import session from "express-session";
 
-//const RELAY_PIN = 4;
-//gpio.init_gpio(RELAY_PIN, gpio.GPIO_MODE_OUTPUT, 1);
+const RELAY_PIN = 4;
+gpio.init_gpio(RELAY_PIN, gpio.GPIO_MODE_OUTPUT, 1);
+const TIME = 1800000;
 
 const app = express();
 
-const db = new sqlite3.Database("./db.sqlite");
+const db = new sqlite3.Database("./instance/db.sqlite");
 
 app.use(bodyParser.json());
 app.use(
@@ -22,12 +23,20 @@ app.use(
   })
 );
 
-/*async function setRelay(amount) {
+function convertAmountToMilliseconds(amount) {
+  // 500원당 1800000밀리초
+  return (amount / 500) * TIME;
+}
+
+async function setRelay(amount) {
   gpio.set_gpio(RELAY_PIN, 0);
 
-  await new Promise((resolve) => setTimeout(resolve, amount));
+  const INACTIVE_TIME = convertAmountToMilliseconds(amount);
+
+  await new Promise((resolve) => setTimeout(resolve, INACTIVE_TIME));
   gpio.set_gpio(RELAY_PIN, 1);
-}*/
+  console.log(INACTIVE_TIME);
+}
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
@@ -38,7 +47,7 @@ app.use("/sandbox-dev/api/v1/payments", router);
 app.post("/sandbox-dev/api/v1/payments/confirm", (req, res) => {
   const reqData = req.body;
 
-  //setRelay(reqData.amount);
+  setRelay(reqData.amount);
 
   console.log(reqData);
 
@@ -286,9 +295,9 @@ async function runQuery(query, params) {
 app.listen(5000, () => console.log("Server is Listening..."));
 
 // 프로그램이 종료되기 전에 GPIO 리소스를 해제
-/*process.on("SIGINT", function () {
+process.on("SIGINT", function () {
   gpio.set_gpio(RELAY_PIN, 1);
   gpio.deinit_gpio(RELAY_PIN);
   console.log(" -> program stopped");
   process.exit();
-});*/
+});
